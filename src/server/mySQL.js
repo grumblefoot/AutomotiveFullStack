368f2faea7c48c38d19113befce821d11c221c97
hyunggilwoo <hyunggilswoo@gmail.com>
2023-05-31 15:08:01 -0700

    Added features to add customer information on MySQL. updated DB connection

--- src/server/mySQL.js
+++ src/server/mySQL.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 const db = require('./db');
 const app = express();
 const port = 3001; // Choose a suitable port number
-
-
+const customerLook = 'api/customerlookup';
+const invoices = '/api/invoices';
 app.use(cors());
 app.use(express.json());
 
@@ -22,7 +22,7 @@ db.connect((error) => {
 });
 
 // Define a route for fetching info from MySQL
-app.get('/api/invoices', (req, res) => {
+app.get(invoices, (req, res) => {
   // put the SQL statement here.
   const query = 'SELECT * FROM invoiceregistry';
   db.query(query, (error, results) => {
@@ -36,7 +36,7 @@ app.get('/api/invoices', (req, res) => {
 });
 
 // Define a route for fetching invoices from MySQL
-app.get('/api/invoices', (req, res) => {
+app.get(invoices, (req, res) => {
   // put the SQL statement here.
   const query = 'SELECT * FROM invoiceregistry';
   db.query(query, (error, results) => {
@@ -48,6 +48,43 @@ app.get('/api/invoices', (req, res) => {
     }
   });
 });
+
+// Define a route for adding customer information on MySQL
+app.post(customerLook, (req, res) => {
+  const { firstName, lastName } = req.body;
+
+  const query = 'INSERT INTO customerregistry (firstName, lastName) VALUES (?, ?)';
+  const values = [firstName, lastName];
+
+  db.query(query, values, (error, result) => {
+    if (error) {
+      console.error('Error adding customer information to MySQL:', error);
+      res.status(500).json({ error: 'Error adding customer information' });
+
+    } else {
+      res.json({ message: 'Customer information added successfully'});
+      console.log("Customer added successfullY"); // printing it
+    }
+  })
+});
+
+// Define a route for retrieving customer information from MySQL
+app.get(customerLook), (req, res) => {
+  const {firstName, lastName } = req.body;
+
+  const query = 'SELECT * FROM customerRegistry as c WHERE c.First_Name = ? AND c.Last_Name = ?';
+  const values = [firstName, lastName];
+
+  db.query(query, values, (error, results) => {
+    if (error) {
+      console.error('Error retrieving customer information from MySQL:', error);
+      res.status(500).json({ error: 'Error retrieving customer information' });
+
+    } else {
+      res.json(results);
+    }
+  })
+}
 
 // Start the server
 app.listen(port, () => {
