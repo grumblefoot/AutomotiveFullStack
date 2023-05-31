4f8ed5df35d29c6cf3d4eb2610a88f9a39d0068f
grumblefoot <70971715+grumblefoot@users.noreply.github.com>
2023-05-31 12:00:42 -0700

    Worked on openTicket page

---
+++ src/pages/Open Tickets/BuildTicket/tickets.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import BuildTicket from './buildtickets';
+
+const Tickets = ({ tickets, onBuildTicket }) => {
+  const handleBuildTicketClick = () => {
+    onBuildTicket();
+  };
+
+  return (
+    <div>
+      {tickets.length > 0 ? (
+        <div>
+          <h2>Submitted Forms:</h2>
+          <ul>
+            {tickets.map((ticket, index) => (
+              <li key={index}>
+                First Name: {ticket.firstName}, Last Name: {ticket.lastName}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <p>ADD TICKETS!</p>
+          <button onClick={handleBuildTicketClick}>Build Ticket</button>
+        </div>
+      )}
+
+      <BuildTicket />
+    </div>
+  );
+};
+
+export default Tickets;
