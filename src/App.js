75c35d7f2eaebc2dae471f47b1cbcafb2078e9f0
grumblefoot <70971715+grumblefoot@users.noreply.github.com>
2023-06-01 16:36:21 -0700

    Work on version 0.1 build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './App.css';
-import HomePage from './pages/HomePage';
-import Services from './pages/Services';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import Services from "./pages/Services";
 import Layout from "./pages/Layout";
 import Invoice from "./pages/Invoice/invoice";
 import CustomerLookup from "./pages/Customer Lookup/customerlookup";
 import IntakeForm from "./pages/IntakeForm/intakeform";
 import OpenTickets from "./pages/Open Tickets/opentickets";
+import server from "./server/server"; // Import the backend file
+
+// Establish MySQL connection
+server.db.connect((error) => {
+  if (error) {
+    console.error("Error connecting to MySQL database:", error);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
 
 function App() {
   return (
@@ -16,15 +26,14 @@ function App() {
           <Route element={<Layout />}>
             <Route path="/" element={<HomePage />} />
             <Route path="/Services" element={<Services />} />
-            <Route path="/Invoice" element = {<Invoice />} />
-            <Route path="/CustomerLookup" element = {<CustomerLookup />} />
-            <Route path="/IntakeForm" element = {<IntakeForm />} />
-            <Route path="/OpenTickets" element = {<OpenTickets />} />
+            <Route path="/Invoice" element={<Invoice />} />
+            <Route path="/CustomerLookup" element={<CustomerLookup />} />
+            <Route path="/IntakeForm" element={<IntakeForm />} />
+            <Route path="/OpenTickets" element={<OpenTickets />} />
           </Route>
         </Routes>
       </BrowserRouter>
     </div>
-    
   );
 }
 
